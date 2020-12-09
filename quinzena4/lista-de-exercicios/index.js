ea3fa4a808b429294b9feda67273599cd7508042
marivellar <mariana.ralleva@gmail.com>
2020-12-09 16:04:57 -0300

    Exercícios de interpretação de código

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,36 @@
+//---------- Exercícios de interpretação de texto----------//
+
+// Exercício 1
+
+/*
+    O código pede para o usuário inserir a cotação do dólar e tranforma em uma variável tipo número.
+    A função é chamada dando o valor 100 de parâmetro para a função, que calcula o valor em reais de 100 dólares.
+    IMPRESSÃO CONSOLE: Se o usuário digitar 6, a impressão no console será R$600.
+ */
+
+
+ // Exercício 2
+
+ /*
+    O código faz um cálculo para cada tipo de investimento a patir de um valor inicial, então retorna o valor total após o investimento.
+    Ao chamar a função o código atribui os parâmetros "Ações" e 150 para a variável novoMontante, como o argumento se encaixa em um case ele retorna o valor 165.
+    Já a variável segundoMontante tem um argumento que não se encaixa no case, portanto é retornado uma mensagem com o texto "TIPO DE INVESTIMENTO INFORMADO INCORRETO"
+*/
+
+
+ // Exercício 3
+
+ /*
+    O código cria duas arrays vazias e uma array com vários números.
+    Ele percorre os itens da array e verifica se o número é par.
+    Armazena os números pares na array1 e os ímpares na array2.
+    IMPRESSÃO CONSOLE: Quantidade total de números 14 // 6 // 8
+ */
+
+
+ // Exercício 4
+
+ /*
+    O código percorre os números de uma array e verifica se o número é menor que o infinito, e se é maior que 0.
+    IMPRESSÃO NO CONSOLE: 1590  1590
+ */
