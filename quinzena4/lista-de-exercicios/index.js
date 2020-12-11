df9774797ffbf743a2da2cc1a3282baa8dbb69dc
marivellar <mariana.ralleva@gmail.com>
2020-12-11 00:43:45 -0300

    Exercícios de lógica de programação

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -1,4 +1,4 @@
-//---------- Exercícios de interpretação de texto----------//
+//---------- Exercícios de interpretação de código----------//
 
 // Exercício 1
 
@@ -34,3 +34,115 @@
     O código percorre os números de uma array e verifica se o número é menor que o infinito, e se é maior que 0.
     IMPRESSÃO NO CONSOLE: 1590  1590
  */
+
+
+ //---------- Exercícios de lógica de programação ----------//
+
+
+ // Exercício 1
+
+
+ /* Maneira 1
+ *
+let numeros = 0
+ 
+ while (numeros <= 5) {
+    console.log(numeros)
+    numeros++
+ }
+ */
+
+
+/* Maneira 2
+ * 
+ for (let num=0; num <=5; num++) {
+    console.log(num)
+ }
+*/
+
+
+/* Maneira 3
+ * 
+  const array = [10, 20, 30, 40, 50]
+ for (let numero of array) {
+    console.log(numero)
+ }
+ */
+
+
+ // Exercício 2
+
+ /*
+ * a. false
+ * b. false
+ * c. true
+ * d. true
+ * e. true
+ */ 
+
+
+ // Exercício 3
+
+
+/**
+ 
+ let qtdDeNumPares = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+ const n = prompt("Digite um número")
+
+ while (n <= 10) {
+    qtdDeNumPares.splice(n, 10-n)
+    console.log(qtdDeNumPares)
+    n++
+ }
+
+ */
+
+
+ // Exercício 4
+
+ /*
+
+ function trigonometria(a, b, c) {
+    if ((a === b) && (b === c)) {
+       console.log("Equilátero")
+    }
+    else if ((a === b) || (b === c) || (a === c)){
+      console.log("Isóceles")
+    } else {
+       console.log("Escaleno")
+    }
+ }
+
+ trigonometria(10, 10, 20)
+ 
+ */
+
+
+ // Exercício 5
+
+ /**
+  
+ const numero1 = prompt("Digite um número")
+ const numero2 = prompt("Digite um número")
+
+ if (numero1 > numero2) {
+    console.log("O maior é:", numero1)
+    console.log("A diferença entre eles é", numero1-numero2)
+ } else if (numero2 > numero1) {
+      console.log("O maior é:", numero2)
+      console.log("A diferença entre eles é", numero2-numero1)
+ } 
+ 
+if (numero1 % numero2 === 0) {
+ console.log(numero1, "é divisível por ", numero2)
+} else if (numero1 % numero2 !== 0) {
+   console.log(numero1, "não é divisível por ", numero2)
+}
+
+if (numero2 % numero1 === 0) {
+   console.log(numero2, "é divisível por ", numero1)
+  } else if (numero1 % numero2 !== 0) {
+     console.log(numero2, "não é divisível por ", numero1)
+  }
+
+*/
